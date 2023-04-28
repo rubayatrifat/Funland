3f0a7a1016a5ef8a3da48f8079eddce75c498f92
rubayatrifat <rubayatislam17@gmail.com>
2023-04-28 11:07:43 +0600

    Working...

--- assest/js/main.js
+++ assest/js/main.js
@@ -70,10 +70,121 @@ async function loadQuestion() {
     showQuestion(data.results[0])
 }
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 
 function showQuestion(data) {
     let correctAns = data.correct_answer;
-    console.log(correctAns)
+    let incorrectAns = data.incorrect_answers;
+    let optionsList = incorrectAns;
+    optionsList.splice(Math.floor(Math.random() * (incorrectAns.lenght + 1)), 0, correctAns)
+    
+    shuffleArray(optionsList)
 }
 
 loadQuestion()
+
+// For Math Area
+
+const ansElm = document.getElementById('ans-input')
+const subElm = document.getElementById('sub-input')
+const firstValue = document.getElementById('first-value')
+const lastValue = document.getElementById('last-value')
+const theSumSign = document.querySelector('.sum')
+const minus = document.getElementById('minus')
+const multiply = document.getElementById('into')
+const substract = document.getElementById('subs')
+const next = document.getElementById('next')
+const alertElm = document.querySelector('.alert')
+
+let rand1
+let rand2
+let getRandomSign
+let defaultSign
+let mainAns
+
+next.disabled = true
+
+window.addEventListener('DOMContentLoaded', showRands)
+
+function showRands() {
+  rand1 = Math.ceil((Math.random() * 15) + 1)
+  rand2 = Math.ceil((Math.random() * 9) + 1)
+  
+  firstValue.innerText = `${rand1}`
+  lastValue.innerText = `${rand2}`
+  
+  getRandomSign = Math.ceil((Math.random()) * 3)
+  
+  if (minus.checked && getRandomSign === 1) {
+    defaultSign = '-'
+    mainAns = rand1 - rand2
+  } else if (multiply.checked && getRandomSign === 2) {
+    defaultSign = '×'
+    mainAns = rand1 * rand2
+  } else if (substract.checked && getRandomSign === 3) {
+    defaultSign = '÷'
+    mainAns = rand1 / rand2
+  } else {
+    defaultSign = '+'
+    mainAns = rand1 + rand2
+  }
+  
+  theSumSign.innerText = defaultSign
+}
+
+next.addEventListener('click', (evt) => {
+  evt.preventDefault()
+  
+  showRands()
+  
+  next.disabled = true
+})
+
+function clearAlert() {
+  alertElm.classList.remove('danger')
+  alertElm.classList.remove('success')
+  alertElm.innerText = ''
+}
+
+subElm.addEventListener('click', () => {
+  const ansValue = ansElm.value
+  
+  if (ansValue === '') {
+    alertElm.classList.remove('success')
+    alertElm.innerText = 'Please fill out the input field.'
+    alertElm.classList.add('danger')
+    setTimeout(() => {
+      clearAlert()
+    }, 2000)
+    
+    return
+  }
+  
+  if (mainAns == ansValue) {
+    alertElm.classList.remove('danger')
+    alertElm.innerText = 'You are correct. Press Next for another.'
+    alertElm.classList.add('success')
+    setTimeout(() => {
+      clearAlert()
+    }, 2000)
+    
+    ansElm.value = ''
+    next.disabled = false
+  } else {
+    alertElm.classList.remove('success')
+    alertElm.innerText = 'You are wrong!'
+    alertElm.classList.add('danger')
+    setTimeout(() => {
+      clearAlert()
+    }, 2000)
+    
+    ansElm.value = ''
+  }
+})
+
